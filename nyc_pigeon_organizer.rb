359935982c82854a40d28583823eba35ccf8e863
Cedric Patton <ceddyp117@gmail.com>
2020-07-03 07:50:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,9 +11,9 @@ def nyc_pigeon_organizer(data)
         
         if !final_array[name]
           final_array[name] = {}
-      binding.pry 
         end
-        
+      if !final_array[name][key]
+        final_array[name][key] = []
     end 
   end
  binding.pry
